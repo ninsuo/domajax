67bc861ebd9dde8cc056347db56b0ed2baa1056a
alain <alain@192.168.1.6>
2014-08-05 22:49:08 +0200

    removed useless variable

--- src/Fuz/DomAjaxBundle/DependencyInjection/FuzDomAjaxExtension.php
+++ src/Fuz/DomAjaxBundle/DependencyInjection/FuzDomAjaxExtension.php
@@ -19,8 +19,6 @@ class FuzDomAjaxExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
