8a96c312d23853171a979d14af0f96a0fb1acfda
Alain <ninsuo@gmail.com>
2014-10-03 00:33:13 +0200

    updated dependancies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,19 +5,21 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+
     public function registerBundles()
     {
-        $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
-            new Fuz\DomAjaxBundle\FuzDomAjaxBundle(),
+        $bundles = array (
+                new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+                new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+                new Symfony\Bundle\TwigBundle\TwigBundle(),
+                new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+                new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+                new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
+                new Fuz\DomAjaxBundle\FuzDomAjaxBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array ('dev', 'test')))
+        {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
         }
@@ -27,6 +29,7 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
     }
+
 }
